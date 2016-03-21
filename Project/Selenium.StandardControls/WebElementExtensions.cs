a4304fb7174c902c0137e0a0e697ca3f04840061
石川達也 <ishikawa-tatsuya@codeer.co.jp>
2016-03-21 14:10:27 +0900

    Add WebElementExtensions.

---
+++ Project/Selenium.StandardControls/WebElementExtensions.cs
@@ -0,0 +1,12 @@
+﻿using OpenQA.Selenium;
+
+namespace Selenium.StandardControls
+{
+    public static class WebElementExtensions
+    {
+        public static IWebElement GetParent(this IWebElement element)
+        {
+            return element.FindElement(By.XPath("parent::node()"));
+        }
+    }
+}
