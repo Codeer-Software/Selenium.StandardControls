53c810c18a395cc9eeef8948ee376dca533109c2
Ishikawa-Tatsuya <stone.guitar.777@gmail.com>
2021-05-14 10:43:13 +0900

    add click to label.

--- Project/Selenium.StandardControls/LabelDriver.cs
+++ Project/Selenium.StandardControls/LabelDriver.cs
@@ -1,6 +1,8 @@
 ﻿using OpenQA.Selenium;
+using Selenium.StandardControls.AdjustBrowser;
 using Selenium.StandardControls.PageObjectUtility;
 using Selenium.StandardControls.TestAssistant.GeneratorToolKit;
+using System;
 
 namespace Selenium.StandardControls
 {
@@ -10,15 +12,39 @@ namespace Selenium.StandardControls
     public class LabelDriver : ControlDriverBase
     {
         /// <summary>
+        /// Wait for end of the time of Invoke
+        /// </summary>
+        public Action Wait { get; set; }
+
+        /// <summary>
         /// Constructor
         /// </summary>
         /// <param name="element">Element for generating the driver</param>
         public LabelDriver(IWebElement element) : base(element) { }
 
         /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="element">Element for generating the driver</param>
+        /// <param name="wait">Wait for end of the time of Invoke</param>
+        public LabelDriver(IWebElement element, Action wait = null) : base(element) { Wait = wait; }
+
+        /// <summary>
         /// Label Text
         /// </summary>
         public string Text => Element.Text;
+
+        /// <summary>
+        /// Click the label
+        /// </summary>
+        public void Click()
+        {
+            //It does not move when you are viewing Show 's only part of the button .
+            Element.Show();
+            Element.Focus();
+            Element.ClickEx();
+            Wait?.Invoke();
+        }
 
         /// <summary>
         /// Constructor
@@ -31,5 +57,15 @@ namespace Selenium.StandardControls
         /// </summary>
         [TargetElementInfo]
         public static TargetElementInfo TargetElement => new TargetElementInfo("label");
+
+        [CaptureCodeGenerator]
+        public string GetWebElementCaptureGenerator()
+        {
+            return $@"
+                    element.addEventListener('click', function() {{ 
+                      var name = __codeerTestAssistantPro.getElementName(this);
+                      __codeerTestAssistantPro.pushCode(name + '.Click();');
+                    }}, false);";
+        }
     }
 }
