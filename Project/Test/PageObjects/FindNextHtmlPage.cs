d946a738f3121f81da2f57bb9bb91ad17311504e
yutaka <shigure@refy.net>
2021-05-18 14:02:09 +0900

    fix findNextElement and implement tests

---
+++ Project/Test/PageObjects/FindNextHtmlPage.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using OpenQA.Selenium;
+using Selenium.StandardControls;
+using Selenium.StandardControls.PageObjectUtility;
+using Selenium.StandardControls.TestAssistant.GeneratorToolKit;
+
+namespace Test.PageObjects
+{
+    public class FindNextHtmlPage : PageBase
+    {
+        public TextBoxDriver Email => ById("email").Wait();
+        public TextBoxDriver Address => ById("address").Wait();
+        public TextBoxDriver Tel => ById("tel").Wait();
+        public AnchorDriver Link1 => ById("link1").Wait();
+        public AnchorDriver Link2 => ById("link2").Wait();
+        public AnchorDriver Link3 => ById("link3").Wait();
+
+        public FindNextHtmlPage(IWebDriver driver) : base(driver) { }
+
+        public static FindNextHtmlPage Open(IWebDriver driver)
+        {
+            driver.Url = Path.GetFullPath("../../FindNext.html");
+            return new FindNextHtmlPage(driver);
+        }
+    }
+
+    public static class FindNextHtmlPageExtensions
+    {
+        [PageObjectIdentify(UrlComapreType.Contains, "FindNext.html")]
+        public static FindNextHtmlPage AttachControlsHtml(this IWebDriver driver) => new FindNextHtmlPage(driver);
+    }
+}
--- Project/Test/TestFindNext.cs
+++ Project/Test/TestFindNext.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Firefox;
 using Selenium.StandardControls;
 using Test.PageObjects;
 
@@ -11,13 +10,15 @@ namespace Test
     public class TestFindNext
     {
         ChromeDriver _driver;
-        ControlsHtmlPage _page;
+        FindNextHtmlPage _page;
+        private IWebElement _anchorElement;
 
         [TestInitialize]
         public void TestInitialize()
         {
             _driver = new ChromeDriver();
-            _page = ControlsHtmlPage.Open(_driver);
+            _page = FindNextHtmlPage.Open(_driver);
+            _anchorElement = _page.ById("td_address").Find();
         }
 
         [TestCleanup]
@@ -27,11 +28,81 @@ namespace Test
         }
 
         [TestMethod]
-        public void Test1()
+        public void ById()
         {
-            _page.Label_Title.Text.Is("Title Controls");
-            var x = _page.CheckBox_Cottage.Element.FindNextElement(By.Id("codeer"));
-
+            var e1 = _anchorElement.FindNextElement(By.Id("email"));
+            Assert.IsNull(e1);
+            var e2 = _anchorElement.FindNextElement(By.Id("tel"));
+            Assert.IsNotNull(e2);
+            new TextBoxDriver(e2).Edit("09012345678");
+            Assert.AreEqual("09012345678", _page.Tel.Text);
+        }
+        [TestMethod]
+        public void ByName()
+        {
+            var e1 = _anchorElement.FindNextElement(By.Name("email"));
+            Assert.IsNull(e1);
+            var e2 = _anchorElement.FindNextElement(By.Name("tel"));
+            Assert.IsNotNull(e2);
+            new TextBoxDriver(e2).Edit("09012345678");
+            Assert.AreEqual("09012345678", _page.Tel.Text);
+        }
+        [TestMethod]
+        public void ByClassName()
+        {
+            var e1 = _anchorElement.FindNextElement(By.ClassName("element-before"));
+            Assert.IsNull(e1);
+            var e2 = _anchorElement.FindNextElement(By.ClassName("element-after"));
+            new TextBoxDriver(e2).Edit("09012345678");
+            Assert.AreEqual("09012345678", _page.Tel.Text);
+        }
+        [TestMethod]
+        public void ByCssSelector()
+        {
+            var e1 = _anchorElement.FindNextElement(By.CssSelector("input[type='email']"));
+            Assert.IsNull(e1);
+            var e2 = _anchorElement.FindNextElement(By.CssSelector("input[type='tel']"));
+            new TextBoxDriver(e2).Edit("09012345678");
+            Assert.AreEqual("09012345678", _page.Tel.Text);
+        }
+        [TestMethod]
+        public void ByTagName()
+        {
+            var e1 = _anchorElement.FindNextElement(By.TagName("x-element"));
+            Assert.IsNull(e1);
+            var e2 = _anchorElement.FindNextElement(By.TagName("input"));
+            Assert.IsNotNull(e2);
+            new TextBoxDriver(e2).Edit("Tokyo, Shinjuku, 1-1");
+            Assert.AreEqual("Tokyo, Shinjuku, 1-1", _page.Address.Text);
+        }
+        [TestMethod]
+        public void XPath()
+        {
+            var e1 = _anchorElement.FindNextElement(By.XPath("//input[@type='email']"));
+            Assert.IsNull(e1);
+            var e2 = _anchorElement.FindNextElement(By.XPath("//input[@type='tel']"));
+            new TextBoxDriver(e2).Edit("09012345678");
+            Assert.AreEqual("09012345678", _page.Tel.Text);
+        }
+        [TestMethod]
+        public void LinkText()
+        {
+            var anchorElement = _page.ById("td_link2").Find();
+            var e1 = anchorElement.FindNextElement(By.LinkText("Link1"));
+            Assert.IsNull(e1);
+            var e2 = anchorElement.FindNextElement(By.LinkText("Link3"));
+            Assert.IsNotNull(e2);
+            Assert.AreEqual(_page.Link3.Element, e2);
+        }
+        [TestMethod]
+        public void PartialLinkText()
+        {
+            var anchorElement = _page.ById("td_link2").Find();
+            var e1 = anchorElement.FindNextElement(By.PartialLinkText("1"));
+            Assert.IsNull(e1);
+            var e2 = anchorElement.FindNextElement(By.PartialLinkText("3"));
+            Assert.IsNotNull(e2);
+            Assert.AreEqual(_page.Link3.Element, e2);
         }
     }
 }
