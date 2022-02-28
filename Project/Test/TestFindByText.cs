3847241e936e7589c28cd676461232c58afe05ca
Ishikawa-Tatsuya <stone.guitar.777@gmail.com>
2022-02-28 17:21:19 +0900

    Add Test.

---
+++ Project/Test/TestFindByText.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Selenium.StandardControls;
+using System.IO;
+using Test.PageObjects;
+
+namespace Test
+{
+
+    [TestClass]
+    public class TestFindByText
+    {
+        ChromeDriver _driver;
+        ControlsHtmlPage _page;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _driver = new ChromeDriver();
+            _page = ControlsHtmlPage.Open(_driver);
+        }
+
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            _driver.Dispose();
+        }
+
+        [TestMethod]
+        public void ByText()
+        {
+            LabelDriver label = _page.ByText("Name：");
+            CheckBoxDriver checkBox = _page.ByText("Cell phone");
+            ButtonDriver button = _page.ByText("JS");
+        }
+    }
+}
