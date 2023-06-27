bed7ffa2414661830ead4831975b9edc0b4db928
石川達也 <ishikawa-tatsuya@codeer.co.jp>
2023-06-27 16:08:08 +0900

    Add IWebDriverCreator. Update Selenium version.

---
+++ Project/Selenium.StandardControls/TestAssistant.GeneratorToolKit/IWebDriverCreator.cs
@@ -0,0 +1,14 @@
+﻿namespace Selenium.StandardControls.TestAssistant.GeneratorToolKit
+{
+    /// <summary>
+    /// Interface for customizing the creation of WebDriver used by TestAssistantPro.
+    /// </summary>
+    public interface IWebDriverCreator
+    {
+        /// <summary>
+        /// Create a WebDriver.
+        /// </summary>
+        /// <returns>IWebDriver</returns>
+        object CreateDriver();
+    }
+}
